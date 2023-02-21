c504e293c732ef4c616521790260c4a5b66b150a
axcm19 <afonsoxcm@gmail.com>
2023-02-21 22:05:30 +0000

    programa 'second_main' para imprimir as 3 tabelas

---
+++ TPC_1/Resolucao_TPC_1/second_main.py
@@ -0,0 +1,45 @@
+from listaDoentes import Lista_Doentes
+
+# programa secundário que imprime as tabelas das distribuições sem ocorrer interação com o utilizador
+
+def main():
+    # abrir o ficheiro myheart.csv e criar o catalogo principal de doentes
+    catalogo_doentes = Lista_Doentes()
+    catalogo_doentes.parse("myheart.csv")
+
+    # construção da UI
+
+    print(" ")
+    print("------------------------------ TPC 1 ------------------------------")
+    print(" ")
+    print("Calculando a distribuição da doença por sexo...")
+    print("Calculando a distribuição da doença por escalões etários...")
+    print("Calculando a distribuição da doença por níveis de colesterol...")
+    print(" ")
+    print(" ")
+    print(" ")
+    print("*** RESULTADOS ***")
+    print(" ")
+
+    print(f"TOTAL DE DOENTES = {catalogo_doentes.total_doentes()}")
+    print(" ")
+
+    print("---> Tabela da distribuição da doença por sexo")
+    print(" ")
+    catalogo_doentes.tabela_sexo()
+    print(" ")
+
+    print("---> Tabela da distribuição da doença por escalões etários (falta implementar a tabela!!!)")
+    print(" ")
+    catalogo_doentes.calcula_dist_etaria()
+    print(" ")
+
+    print("---> Tabela da distribuição da doença por níveis de colesterol...")
+    print(" ")
+    catalogo_doentes.tabela_colestrol()
+    print(" ")
+
+    print("-------------------------------------------------------------------")
+
+if __name__ == "__main__":
+    main()
