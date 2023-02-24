b811f549093e1d4261dad9ef0ac3510c925f8d93
axcm19 <afonsoxcm@gmail.com>
2023-02-24 17:55:53 +0000

    continuação da resolução do tpc2

--- TPC_2/Resolucao_TPC_2/main.py
+++ TPC_2/Resolucao_TPC_2/main.py
@@ -5,21 +5,49 @@ def main():
     print(" ")
     print("------------------------------ TPC 2 ------------------------------")
     print("Insira texto:")
+    print(" ")
+    print("  = --> Devolve o valor da soma total")
+    print("  OFF (seja uppercase ou lowercase) --> Interrompe a soma")
+    print("  ON (seja uppercase ou lowercase) --> Retoma a soma")
     print("-------------------------------------------------------------------")
     print(" ")
 
     texto = input()
     soma = 0
+    estado = 1 # 1 -> on, 0 -> off
 
     while True:
 
         for carater in texto:
-            if carater.isdigit():
+            lowercase = carater.lower()
+
+            if carater.isdigit() and estado == 1:
                 soma += int(carater)
 
 
-            if carater == "=":
+            if carater == "=" and estado == 1:
                 print(f"Soma total = {soma}")
+
+            #-------------------------------------------------------------------------------------------------
+            # caso seja off
+
+            if lowercase == "o" and texto[+1].lower() == "f" and texto[+1].lower() == "f":
+                estado = 0
+
+            if lowercase == "f" and texto[-1].lower() == "o" and texto[+1].lower() == "f":
+                estado = 0
+
+            if lowercase == "f" and texto[-1].lower() == "o" and texto[+1].lower() == "f":
+                estado = 0
+
+            # -------------------------------------------------------------------------------------------------
+            # caso seja on
+
+            if lowercase == "o" and texto[+1].lower() == "n":
+                estado = 1
+
+            if lowercase == "n" and texto[-1].lower() == "o":
+                estado = 1
 
         texto = input()
 
